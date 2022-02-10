88c7685ddf8e3834ffe7d4a1370d3b303c4e6de1
Kevin Haube <khaube@fearless.tech>
2022-02-10 16:22:12 -0500

    Exploration of an API interface module approach to a network layer with axios

--- frontend-react/src/network/HistoryApi.ts
+++ frontend-react/src/network/HistoryApi.ts
@@ -26,9 +26,27 @@ export type Action = {
     action: string | undefined
 }
 
+/* 
+    Enumerated endpoints keeps things tidy in each API interface we build
+*/
+enum Endpoint {
+    REPORTS = '/api/history/report'
+}
+
+/* 
+    Keeping these out of component code, as well, keeps things
+    pretty clean. Happy devs, happy Kev!
+*/
 const accessToken = getStoredOktaToken();
 const organization = getStoredOrg();
 
+/* 
+    The general idea is an instance per API since headers may vary.
+    
+    We could also set baseURL and response type globally? Though,
+    we have to consider all CRUD operations; do we ever get anything
+    but JSON back? A file, maybe?
+*/
 const historyApi = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
     headers: {
@@ -45,7 +63,7 @@ const historyApi = axios.create({
     @returns Report[]
 */
 const getReports = async () => {
-    const response = await historyApi.get<Report[]>('/api/history/report')
+    const response = await historyApi.get<Report[]>(Endpoint.REPORTS)
     const reports = response.data
     return reports
 }
